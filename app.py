1533e7e0de105b1a133feb52a257608319efe1f8
Hassan Ademola <dewumi205@gmail.com>
2022-02-01 21:28:39 +0100

    added progress bar and rearranged widgets

--- app.py
+++ app.py
@@ -1,58 +1,75 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import time
 
 st.title('Diabnotics')
-st.write('Which of these do you feel today?')
+st.subheader("This is a diagnostic tool that predicts a patient's risk of diabetes")
 
-age=st.slider('How old are you?',0,100,16)
-gender=st.selectbox('Gender', ['Female','Male'])
-st.select_slider('Hey',[1,2,3])
-st.subheader('Which of these symptoms are you experiencing?')
 columns = st.columns(2)
 with columns[0]:
-    polyuria=st.selectbox('Polyuria',['No','Yes'])
+    age=st.slider('How old are you?',0,100,16)
 with columns[1]:
-    polydipsia=st.selectbox('Polydipsia', ['No','Yes'])
+    gender=st.selectbox('Gender', ['Female','Male'])
 
-columns = st.columns(2)
+st.subheader('Which of these symptoms do you have?')
+columns = st.columns(4)
 with columns[0]:
+    polyuria=st.selectbox('Excessive Peeing',['No','Yes'])
+with columns[1]:
+    polydipsia=st.selectbox('Extreme Thirst', ['No','Yes'])
+with columns[2]:
     sudden_weight_loss=st.selectbox('Sudden Weight Loss',['No','Yes'])
-with columns[1]:
+with columns[3]:
     weakness=st.selectbox('Weakness', ['No','Yes'])
           
-columns = st.columns(2)
+columns = st.columns(4)
 with columns[0]:
-    polyphagia=st.selectbox('Polyphagia',['No','Yes'])
+    polyphagia=st.selectbox('Increased Appetite',['No','Yes'])
 with columns[1]:
-    genital_thrush=st.selectbox('Genital Thrush', ['No','Yes'])       
-
-columns = st.columns(2)
-with columns[0]:
+    genital_thrush=st.selectbox('Yeast Infection', ['No','Yes'])       
+with columns[2]:
     visual_blurring=st.selectbox('Blurry Vision',['No','Yes'])
-with columns[1]:
+with columns[3]:
     itching=st.selectbox('Itching', ['No','Yes'])       
 
-columns = st.columns(2)
+columns = st.columns(4)
 with columns[0]:
-    irritability=st.selectbox('Irritability',['No','Yes'])
+    irritability=st.selectbox('Frustration',['No','Yes'])
 with columns[1]:
     delayed_healing=st.selectbox('Delayed Healing', ['No','Yes'])          
-
-columns = st.columns(2)
-with columns[0]:
-    partial_paresis=st.selectbox('Partial Paresis',['No','Yes'])
-with columns[1]:
+with columns[2]:
+    partial_paresis=st.selectbox('Muscle Weakness',['No','Yes'])
+with columns[3]:
     muscle_stiffness=st.selectbox('Muscle Stiffness', ['No','Yes'])     
-
+    
 columns = st.columns(2)
 with columns[0]:
-    alopecia=st.selectbox('Alopecia',['No','Yes'])
+    alopecia=st.selectbox('Hair Loss',['No','Yes'])
 with columns[1]:
     obesity=st.selectbox('Obesity', ['No','Yes'])
 
 features = pd.DataFrame([[age,gender,polyuria,polydipsia,sudden_weight_loss,weakness,polyphagia,genital_thrush,visual_blurring,itching,irritability,delayed_healing,partial_paresis,muscle_stiffness,alopecia,obesity]])
+
+# encode categories
 features.replace(['Yes','Positive','Male','No','Negative','Female'],[1,1,1,0,0,0],inplace=True)
+
+# load trained model
 model = pickle.load(open('model.pkl','rb'))
-prediction = model.predict_proba(features)[:,1][0]
-f'You are {round(prediction*100)}% likely to have diabetes'
+
+# predict probability of the case being diabetic
+probability = model.predict_proba(features)[:,1][0]
+percentage = round(probability*100)
+
+my_bar = st.progress(0)
+for percent_complete in range(percentage):
+     time.sleep(0.02)
+     my_bar.progress(percent_complete + 1)
+if percentage<=50:
+    st.success(f'You are **{percentage}%** likely to have diabetes')
+
+elif 50<percentage<=75:
+    st.warning(f'You are **{percentage}%** likely to have diabetes')
+
+else:
+    st.error(f'You are **{percentage}%** likely to have diabetes')
